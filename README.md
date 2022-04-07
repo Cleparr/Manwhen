87e5c8ca7f418fc8aeee48076b96fe75b2848ab1
Cleparr <32302778+Cleparr@users.noreply.github.com>
2022-04-07 17:49:40 +0200

    New Feature | Add new follow

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Manwhen
- Stay updated with the latest Manga/Manwha scans
+Projet basé sur l'idée d'un TV Time pour les sorties de manga / manwha
+
+WIP
+
+#Set up
+Pour lancer l'environnement virtuel : 
+
+python -m venv venv
+venv\scripts\activate
+
+Dans l'environnement virtuel : 
+
+python -m pip install flask
+python -m pip install sqlalchemy
+python -m pip install flask-sqlalchemy
+python -m pip install requests
+
+python -m pip install tldextract
+
+# Pour coder en python dans le Venv
--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from click import echo
 from flask import Flask, render_template_string, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+import requests
+from urllib.parse import urlparse
+import tldextract
 
 
 app = Flask(__name__)
@@ -13,7 +16,10 @@ db = SQLAlchemy(app)
 class Manga(db.Model):
     __tablename__ = 'manga'
     id = db.Column(db.Integer, primary_key= True)
-    manga_chapters = db.relationship("MangaChapter", backref="manga", lazy=True)
+    manga_chapters = db.relationship("MangaChapter", 
+                                    order_by = "desc(MangaChapter.chapter_number)",
+                                    backref="manga", 
+                                    lazy=True)
 
 
     name = db.Column(db.String(120), nullable= False, unique = True)
@@ -44,12 +50,12 @@ class MangaChapter(db.Model):
 def home():
     rule = str(request.url_rule)
 
-    manga = Manga.query.all()
+    mangas_list = Manga.query.all()
 
     
     return render_template('home.html',
         rule = rule,
-        manga = manga )
+        mangas_list = mangas_list )
 
 
 @app.route("/abo")
@@ -58,4 +64,58 @@ def mylist_abo():
 
     return render_template('abo.html',
     rule = rule)
+
+
+@app.route("/manga/<manga_id>")
+def DynamicUrl(manga_id):
+    rule = str(request.url_rule)
+
+    manga = Manga.query.get(manga_id)
+    if manga is None:
+        return redirect("/")
+    
+    return render_template('manga.html',
+    rule = rule,
+    manga = manga)
+
+
+@app.route("/add", methods=['GET'])
+def index():
+    rule = str(request.url_rule)
+    
+    return render_template('add_follow.html',
+        rule = rule)
+
+@app.route("/add", methods=['POST'])
+def add_follow():
+    rule = str(request.url_rule)
+
+    manga_name = request.form['manga_name']
+    manga_url_lecture = request.form['manga_url']
+    manga_cover_url = request.form['manga_cover_url']
+
+    # Checker si le nom existe déjà en base
+    manga_name_check = db.session.query(Manga.id).filter_by(name=manga_name).scalar() is not None
+       
+    #Checker si l'url arrive bien sur une page que je connais : https://www.scan-vf.net/
+
+    domain_url = tldextract.extract(manga_url_lecture)
+
+    echo(domain_url.domain)
+
+    if domain_url.domain != 'scan-vf' :
+        return redirect("/add")
+    
+        
+    manga_to_add = Manga(name = manga_name,
+                            url = manga_url_lecture,
+                            cover = manga_cover_url)
+    
+    db.session.add(manga_to_add)
+
+    db.session.commit()
+
+    #Retourner un message visuel que c'est ajouté en base // Qu'il y a eu une erreur (la spécifier)
+
+    return redirect("/")
 
